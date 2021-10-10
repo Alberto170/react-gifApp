d6451be55757bde84b2b2f9b88c9056ff811491d
Eltunas170 <beto.martinez.velazquez@gmail.com>
2021-10-10 14:45:22 -0700

    First commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,14 @@
+export const getGifs = async(category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=eOtT1ycTr7ulM8EIN8GWJXKy9JnEx3d1`;
+    const resp = await fetch(url);
+    const {data} = await resp.json();
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    }); 
+
+    return gifs;
+}
